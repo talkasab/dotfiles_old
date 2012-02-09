4a4100d206a0ffba487a49ff8ec19a1bd6bd8d6a
Tarik Alkasab <tarik.alkasab@gmail.com>
2012-02-09 15:59:08 -0500

    Replacing IRB with Pry.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Install dotfiles into the home directory
-for file in bash_profile bashrc git-prompt.conf gitconfig irbrc railsrc autotest ackrc tmux.conf
+for file in bash_profile bashrc git-prompt.conf gitconfig pryrc railsrc autotest ackrc tmux.conf
 do
   fromfile="_$file"
   tofile="$HOME/.$file"
